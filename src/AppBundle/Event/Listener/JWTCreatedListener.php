9ef8be9f134f085236283b53b0b90038b05f0402
jim29800 <jbouvier1173@gmail.com>
2018-09-27 20:11:29 +0200

    Ajout de l'id

---
+++ src/AppBundle/Event/Listener/JWTCreatedListener.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace AppBundle\Event\Listener;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    /**
+     * Replaces the data in the generated
+     *
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        /** @var $user \AppBundle\Entity\User */
+        $user = $event->getUser();
+
+        // add new data
+        $payload['id'] = $user->getId();
+        $payload['username'] = $user->getUsername();
+
+        $event->setData($payload);
+    }
+}
