dc9a8cec37ca536061901c4fdff09a4d0c8ccec3
jim29800 <jbouvier1173@gmail.com>
2018-09-27 14:44:25 +0200

    Get Post Put Patch Delete : Product

---
+++ src/AppBundle/Form/ProductType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('title')
+        ->add('description')
+        ->add('image')
+        ->add('ref')
+        ->add('city')
+        ->add('tags')
+        ->add('category')
+        ->add('user');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Product',
+            'csrf_protection' => false
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_product';
+    }
+
+
+}
+
