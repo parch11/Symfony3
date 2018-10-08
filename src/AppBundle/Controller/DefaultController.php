43fdac339bf2c7ae70ac3c551082f35b9e19a21a
jim29800 <jbouvier1173@gmail.com>
2018-10-08 14:47:27 +0200

    index admin

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,9 +9,15 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function indexAction()
     {
         return $this->render('index.html.twig');
     }
-
+    /**
+     * @Route("/admin", name="admin_index")
+     */
+    public function adminIndexAction()
+    {
+        return $this->render('Admin/index.html.twig');
+    }
 }
