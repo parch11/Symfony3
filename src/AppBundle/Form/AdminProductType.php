a0c9aae075de0e87e0297da5ef458a21c35e9093
jim29800 <jbouvier1173@gmail.com>
2018-10-09 08:59:58 +0200

    Form bootstrap avec preview de l'image

--- src/AppBundle/Form/AdminProductType.php
+++ src/AppBundle/Form/AdminProductType.php
@@ -15,16 +15,50 @@ class AdminProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('title')
-        ->add('description')
-        ->add('imageFile', VichFileType::class)
-        ->add('updatedAt')
-        ->add('ref')
-        ->add('createdAt')
-        ->add('autoDeleteAt')
-        ->add('city')
-        ->add('tags')
-        ->add('category');
+        ->add('title',null, array(
+            'label' => "Titre de l'annonce",
+            'attr' => array('class' => 'form-control')
+        ))
+        ->add('description', null, array(
+            'label' => "Déscription du produit",
+            'attr' => array('class' => 'form-control', 'rows' => '8')
+        ))
+        ->add('imageFile', VichFileType::class, array(
+            'required' => false,
+        ))
+        ->add('ref', null, array(
+            'label' => "Référence de l'annonce",
+            'attr' => array('class' => 'form-control')
+        ))
+        ->add('autoDeleteAt',null, array(
+            'label' => 'Date de mise hors ligne',
+            'date_widget' => 'single_text',
+            'time_widget' => 'single_text',
+            'format' => 'dd-MM-yyyy HH:mm',
+            'input' => 'datetime',
+            'attr' => array('data-date-format' => 'dd-MM-yyyy HH:mm',
+                // 'class' => 'form-control'
+            ),
+        ))
+        ->add('city', null, array(
+            'label' => "Emplacement du prduit",
+            'attr' => array('class' => 'form-control')
+        ))
+        ->add('tags', null, array(
+            'label' => "Tags liés",
+            'expanded' => true,
+            'multiple' => true,
+            'required' => true,
+            'choice_attr' => function ($choiceValue, $key, $value) {
+                return ['class' => 'form-check-input'];
+            },
+
+        ))
+        ->add('category', null, array(
+            'label' => "Catégorie du produit",
+            'attr' => array('class' => 'form-control')
+        ))
+        ;
     }/**
      * {@inheritdoc}
      */
