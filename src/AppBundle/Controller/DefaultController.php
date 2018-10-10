ffcef91951acf7cdcce443bf0ca07db0c9b83910
jim29800 <jbouvier1173@gmail.com>
2018-10-10 17:22:18 +0200

    modification du mdp de l'user courant

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,10 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
@@ -13,11 +17,41 @@ class DefaultController extends Controller
     {
         return $this->render('index.html.twig');
     }
+
     /**
+     * @Security("is_granted('ROLE_USER')")
      * @Route("/admin", name="admin_index")
      */
     public function adminIndexAction()
     {
         return $this->render('Admin/index.html.twig');
+    }
+
+    /**
+     * Displays a form to edit the password of an existing user entity.
+     *
+     * @Security("is_granted('ROLE_USER')")
+     * @Route("/admin/password", name="user_password")
+     * @Method({"GET", "POST"})
+     */
+    public function passwordAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $this->getUser();
+        $editForm = $this->createForm('AppBundle\Form\AdminUserPasswordType', $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            //hash du password
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirect('/logout');
+        }
+
+        return $this->render('Admin/password.html.twig', array(
+            'user' => $user,
+            'form' => $editForm->createView(),
+        ));
     }
 }
