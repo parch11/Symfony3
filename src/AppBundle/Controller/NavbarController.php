03c2f2e20fbbfea9c4261bc0f3c93fe8776195c8
jim29800 <jbouvier1173@gmail.com>
2018-10-11 11:26:05 +0200

    tags sur navbar

--- src/AppBundle/Controller/NavbarController.php
+++ src/AppBundle/Controller/NavbarController.php
@@ -13,12 +13,14 @@ class NavbarController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $categories = $em->getRepository('AppBundle:Category')->findAll();
+        $tags = $em->getRepository('AppBundle:Tag')->findAll();
         // $currentRoute = $this->container->get('request_stack')->getMasterRequest()->get('_route');
         $currentUrl = $this->container->get('request_stack')->getMasterRequest()->getRequestUri();
 
         return $this->render('Navbar/navbar.html.twig', array(
             'categories' => $categories,
-            'currentUrl' => $currentUrl
+            'currentUrl' => $currentUrl,
+            'tags' => $tags
         ));
     }
 }
---
+++ web/js/navbar.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    var tagList = new List('tag-nav-list', {
+        valueNames: ['tagName'],
+    });
+});
